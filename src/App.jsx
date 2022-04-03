280866fd30332e10c61cf8683b65c35f5924f90f
pumoyuz <50436595+pumoyuz@users.noreply.github.com>
2022-04-03 19:13:07 +0900

    50. JSXで構造を作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,36 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>
+            <span>あああ</span>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+          <li>
+            <span>あああ</span>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <li>
+            <span>あああ</span>
+            <button>戻す</button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
