5e34d3e72f86128c811711efad1d8e327da5d962
pumoyuz <50436595+pumoyuz@users.noreply.github.com>
2022-04-03 19:46:58 +0900

    54. タスクの削除機能

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,13 @@ export const App = () => {
     setTodoText("");
   };
 
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    // 指定されたindexから1つ削除する
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
   return (
     <>
       <div className="input-area">
@@ -33,13 +40,14 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               // reactは差分だけ反映するので、ループの場合は何回目なのか明確にするためにkeyが必須
               <li key={todo}>
                 <span>{todo}</span>
                 <button>完了</button>
-                <button>削除</button>
+                {/* 引数を渡したいときはアロー関数にしないとこの場で関数が実行されてしまう */}
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </li>
             );
           })}
