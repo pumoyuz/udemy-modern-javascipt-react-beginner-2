a0ceb1533c7ce97c1844c7e07d539eaf5d556e59
pumoyuz <50436595+pumoyuz@users.noreply.github.com>
2022-04-03 16:15:09 +0900

    46. 再レンダリングと副作用を知る(useEffect)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 //コンポーネント名はパスカルケースで
 const App = () => {
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+
+  const onClickSwitchShowFlag = () => {
+    // 現在のfaceShowFlagの逆のbooleanを入れる
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        // flagがfalseのときだけtrueにする
+        // じゃないと毎回ステートが変わって再レンダリングが走る
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+  // 空の配列を渡すと最初の1回だけレンダリングされる
+  // 配列を渡すと、その配列が変わるたびに再レンダリングされる
+  // faceShowFlagを監視しないとESLintが警告してくるが、今回はあえてfaceShowFlagを監視させたくないので書かない
 
   return (
     // reactでreturnするときは1つのタグで囲まないといけない
@@ -17,8 +41,11 @@ const App = () => {
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
-
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {/* &&の左がtrueのとき右が計算される */}
+      {faceShowFlag && <p>😃</p>}
     </>
   );
 };
