3f14d388f76cbb364c74225ac3f02afcaedd6391
pumoyuz <50436595+pumoyuz@users.noreply.github.com>
2022-04-03 19:25:34 +0900

    52. Reactでの実装を意識したモックに変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  // 未完了TODO用の配列
+  const [incompleteTodos, setIncompleteTodos] = useState(["あああ", "いいい"]);
+  // 完了TODO用の配列
+  const [completeTodos, setCompleteTodos] = useState(["ううう"]);
   return (
     <>
       <div className="input-area">
@@ -11,25 +15,29 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          <li>
-            <span>あああ</span>
-            <button>完了</button>
-            <button>削除</button>
-          </li>
-          <li>
-            <span>あああ</span>
-            <button>完了</button>
-            <button>削除</button>
-          </li>
+          {incompleteTodos.map((todo) => {
+            return (
+              // reactは差分だけ反映するので、ループの場合は何回目なのか明確にするためにkeyが必須
+              <li key={todo}>
+                <span>{todo}</span>
+                <button>完了</button>
+                <button>削除</button>
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
-          <li>
-            <span>あああ</span>
-            <button>戻す</button>
-          </li>
+          {completeTodos.map((todo) => {
+            return (
+              <li key={todo}>
+                <span>{todo}</span>
+                <button>戻す</button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </>
